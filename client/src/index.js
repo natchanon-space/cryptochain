c53bcc888e2b900566c7215800c9e04b0bf465e9
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2021-06-27 15:07:53 +0700

    frontend

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import { render } from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./components/App";
 import Blocks from "./components/Blocks";
+import ConductTransaction from "./components/ConductTransaction";
+import TransactionPool from "./components/TransacitonPool";
 import "./index.css"; // equipped CSS
 
 render(
@@ -10,6 +12,8 @@ render(
         <Switch>
             <Route exact path="/" component={App} />
             <Route exact path="/blocks" component={Blocks} />
+            <Route exact path="/conduct-transaction" component={ConductTransaction} />
+            <Route exact path="/transaction-pool" component={TransactionPool} />
         </Switch>
     </BrowserRouter>,
     document.getElementById("root")
